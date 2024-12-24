164fce4b718c737a26208f4737a51a3943522f26
adriuus2000 <adrian.lopez.pardina@estudiantat.upc.edu>
2024-12-24 04:04:45 +0100

    Update wav2lpcc.sh

--- scripts/wav2lpcc.sh
+++ scripts/wav2lpcc.sh
@@ -23,8 +23,9 @@ if [[ $# != 3 ]]; then
 fi
 
 lpc_order=$1
-inputfile=$2
-outputfile=$3
+lpcc_order=$2
+inputfile=$3
+outputfile=$4
 
 UBUNTU_SPTK=1
 if [[ $UBUNTU_SPTK == 1 ]]; then
@@ -33,17 +34,19 @@ if [[ $UBUNTU_SPTK == 1 ]]; then
    FRAME="sptk frame"
    WINDOW="sptk window"
    LPC="sptk lpc"
+   LPCC="sptk lpc2c"
 else
    # or install SPTK building it from its source
    X2X="x2x"
    FRAME="frame"
    WINDOW="window"
    LPC="lpc"
+   LPCC="lpc2c"
 fi
 
 # Main command for feature extration
 sox $inputfile -t raw -e signed -b 16 - | $X2X +sf | $FRAME -l 240 -p 80 | $WINDOW -l 240 -L 240 |
-	$LPC -l 240 -m $lpc_order > $base.lp || exit 1
+	$LPC -l 240 -m $lpc_order > $base.lp | $LPCC -m $lpc_order -M $lpcc_order > $base.lpcc || exit 1
    
 
 # Our array files need a header with the number of cols and rows:
